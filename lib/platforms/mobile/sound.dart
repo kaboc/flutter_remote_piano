286c8a00dcc5cc2f79ea899fd00054ca49ba6dc0
Kabo <kaboc.app@gmail.com>
2020-03-05 20:10:58 +0900

    Update of packages and some other tiny changes

--- lib/platforms/mobile/sound.dart
+++ lib/platforms/mobile/sound.dart
@@ -8,14 +8,16 @@ export 'package:flutter_remote_piano/platforms/sound_base.dart';
 const _fontPath = 'assets/sf2/Piano.SF2';
 
 class Sound extends SoundBase {
+  FlutterMidi midi = FlutterMidi();
+
   @override
   void init() {
-    FlutterMidi.unmute();
-    rootBundle.load(_fontPath).then((sf2) => FlutterMidi.prepare(sf2: sf2));
+    midi.unmute();
+    rootBundle.load(_fontPath).then((sf2) => midi.prepare(sf2: sf2));
   }
 
   @override
   void play(int pitch) {
-    FlutterMidi.playMidiNote(midi: pitch);
+    midi.playMidiNote(midi: pitch);
   }
 }
