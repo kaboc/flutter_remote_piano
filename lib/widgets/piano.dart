577ba3a969d0114622134d7f5b8ee29977cbdbd8
Kabo <kaboc.app@gmail.com>
2020-04-11 12:43:29 +0900

    Prevent piano from shrinking when keyboard appears

--- lib/widgets/piano.dart
+++ lib/widgets/piano.dart
@@ -14,6 +14,7 @@ class Piano extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(l(context).appTitle),
         actions: const <Widget>[ConnectionButton()],
